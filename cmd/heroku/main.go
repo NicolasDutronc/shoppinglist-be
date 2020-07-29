cd048cb955fc97b022e441b024a9db7de5fcba07
NicolasDutronc <nicolas.dutronc@orange.fr>
2020-07-29 21:15:19 +0200

    feat(heroku): added Procfile, heroku specific main file and heroku specific build constraints in the go.mod file

---
+++ cmd/heroku/main.go
@@ -0,0 +1,125 @@
+package main
+
+import (
+	"context"
+	"log"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/NicolasDutronc/autokey"
+	"github.com/NicolasDutronc/shoppinglist-be/internal/api"
+	"github.com/NicolasDutronc/shoppinglist-be/internal/config"
+	"github.com/NicolasDutronc/shoppinglist-be/internal/list"
+	"github.com/NicolasDutronc/shoppinglist-be/internal/user"
+	"github.com/NicolasDutronc/shoppinglist-be/pkg/hub"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/mongo/readpref"
+)
+
+func main() {
+	ctx := context.Background()
+
+	// Create an interruption channel
+	quit := make(chan struct{}, 1)
+
+	// build config from the environment
+	conf, err := config.NewConfig("config.yml")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// heroku specific PORT env var
+	herokuPort := os.Getenv("PORT")
+	if herokuPort == "" {
+		log.Fatal("PORT was not set")
+	}
+	conf.Server.Port = herokuPort
+
+	// heroku mongodb uri
+	mongoURI := os.Getenv("MONGODB_URI")
+	if mongoURI == "" {
+		log.Fatal("MONGODB_URI was not set")
+	}
+
+	// database client
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI(mongoURI))
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer client.Disconnect(ctx)
+
+	// database connection test
+	err = client.Ping(ctx, readpref.Primary())
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// get database and collections
+	db := client.Database(conf.Database.Name)
+	listCollection := db.Collection(conf.Database.ListsCollection)
+	userCollection := db.Collection(conf.Database.UsersCollection)
+
+	// create data repositories
+	listRepository := list.NewMongoDBRepository(listCollection)
+	userRepository := user.NewMongoDBRepository(userCollection)
+
+	// create and start hub
+	// get the current lists to create topics
+	currentLists, err := listRepository.FindAllLists(ctx)
+	if err != nil {
+		log.Fatalf("Error getting the current lists : %v", err.Error())
+	}
+	topics := make([]hub.Topic, len(currentLists))
+	for i, list := range currentLists {
+		topics[i] = hub.TopicFromString(list.ID.Hex())
+	}
+	storage := hub.NewInMemoryHubStorage()
+	h, err := hub.NewChannelHub(ctx, storage, topics...)
+	if err != nil {
+		log.Fatal(err)
+	}
+	go h.Run(ctx, quit)
+	defer h.Close(ctx)
+
+	// create and start the key manager
+	manager := autokey.NewManager(conf, conf.KeyConfig.Size, conf.KeyConfig.ValidDuration)
+	go manager.Start(quit)
+	defer manager.Stop()
+
+	// create services
+	listSrv := list.NewService(listRepository, h)
+	userSrv := user.NewService(userRepository, conf)
+
+	// setup routes
+	r := api.SetupRoutes(userSrv, listSrv, h)
+
+	// setup server
+	server := &http.Server{
+		Addr:    conf.BuildServerAdress(),
+		Handler: r,
+	}
+
+	// start server
+	go func() {
+		log.Printf("Listening on %s:%s", conf.Server.Hostname, conf.Server.Port)
+		if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("Server error : %s", err)
+		}
+	}()
+
+	// block until a signal is received
+	<-quit
+
+	log.Println("Shuting down server...")
+
+	ctx, cancel := context.WithTimeout(ctx, 5*time.Second)
+	defer cancel()
+
+	if err := server.Shutdown(ctx); err != nil {
+		log.Fatalf("Server forced to shutdown before timeout : %s", err)
+	}
+
+	log.Println("Server exiting")
+}
