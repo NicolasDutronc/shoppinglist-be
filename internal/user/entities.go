912af0eb2ca4ae335e5d1972fc6f33a73c546f09
NicolasDutronc <nicolas.dutronc@orange.fr>
2020-08-12 17:42:57 +0200

    feat(user) : added a constructor and a Can method for authorization

--- internal/user/entities.go
+++ internal/user/entities.go
@@ -1,10 +1,51 @@
 package user
 
-import "github.com/NicolasDutronc/shoppinglist-be/internal/common"
+import (
+	"fmt"
+	"time"
+
+	"github.com/NicolasDutronc/shoppinglist-be/internal/common"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
 
 // User represents a user
 type User struct {
 	common.BaseModel `bson:",inline"`
-	Name             string `bson:"name" json:"name"`
-	Password         string `bson:"password" json:"password"`
+	Name             string        `bson:"name" json:"name"`
+	Password         string        `bson:"password"`
+	Permissions      []*Permission `bson:"permissions"`
+}
+
+// NewUser is a User constructor
+func NewUser(name string, password string, permissions ...*Permission) *User {
+	return &User{
+		BaseModel: common.BaseModel{
+			ID:        primitive.NewObjectID(),
+			CreatedAt: time.Now(),
+			UpdatedAt: time.Now(),
+		},
+		Name:        name,
+		Password:    password,
+		Permissions: permissions,
+	}
+}
+
+// Can checks if the user can do the action on the resource
+func (u *User) Can(action string, resourceID string) error {
+	for _, p := range u.Permissions {
+		if p.Action == "*" && p.ResourceID == "*" {
+			return nil
+		}
+		if p.Action == action && p.ResourceID == resourceID {
+			return nil
+		}
+	}
+
+	return fmt.Errorf("User %s cannot %s on resource %s", u.Name, action, resourceID)
+}
+
+// Permission is a triplet that represents the right to do some action on a resource
+type Permission struct {
+	ResourceID string `bson:"resource_id"`
+	Action     string `bson:"action"`
 }
